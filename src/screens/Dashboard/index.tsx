323514f3918addc1bbf1772d04dd0724559b96c8
Rafinhaa <rafinhaa@me.com>
2021-11-23 12:25:19 -0300

    Chapter II - Styled Components - Criando componentes com o Styled Components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+	Text,
+} from 'react-native';
+
+import { Container, Title } from './styles';
+
+// import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+	return (
+		<Container>
+			<Title>Dashboard</Title>
+		</Container>
+	);
+}
+
+export default Dashboard;
