c6a60c5e636912c6f9f082147ae5ac692e8ee0f2
Rafinhaa <rafinhaa@me.com>
2021-11-23 14:51:14 -0300

    Chapter II - Styled Components - Utilizando fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
 
 import Dashboard from './src/screens/Dashboard';
 import theme  from './src/global/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ // aguarda o carregamento das fontes
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) { // se não carregou, retorna um loading
+    return <AppLoading />;
+  }
+
   return (
+
     <ThemeProvider theme={theme}>
       <Dashboard />
     </ThemeProvider>
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import {
-	Text,
-} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
 
 import { Container, Title } from './styles';
-
-// import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
 	return (
