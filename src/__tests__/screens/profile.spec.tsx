ff16092830c2b97d18b51aff4c81ddc18ae8ea55
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-03-15 09:37:49 -0300

    Chapter V - Testes Unitátios - Suite de Testes

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,23 +3,24 @@ import { render } from "@testing-library/react-native";
 
 import Profile from "../../screens/Profile";
 
-test("check if show correctly user input name placeholder", () => {
-  const { getByPlaceholderText } = render(<Profile />);
-  const inputName = getByPlaceholderText("Nome");
-  expect(inputName.props.placeholder).toBeTruthy(); // Verifica se foi encontrado
-});
+describe("Profile Screen", () => {
+  it("should have placeholder correctly in input name", () => {
+    const { getByPlaceholderText } = render(<Profile />);
+    const inputName = getByPlaceholderText("Nome");
+    expect(inputName.props.placeholder).toBeTruthy(); // Verifica se foi encontrado
+  });
+  it("should be loaded user data", () => {
+    const { getByTestId } = render(<Profile />);
+    const inputName = getByTestId("input-name");
+    const inputSurname = getByTestId("input-surname");
 
-test("checks if user data has been loaded", () => {
-  const { getByTestId } = render(<Profile />);
-  const inputName = getByTestId("input-name");
-  const inputSurname = getByTestId("input-surname");
+    expect(inputName.props.value).toEqual("Rafael");
+    expect(inputSurname.props.value).toEqual("Soncine");
+  });
 
-  expect(inputName.props.value).toEqual("Rafael");
-  expect(inputSurname.props.value).toEqual("Soncine");
-});
-
-test("check if title render correctly", () => {
-  const { getByTestId } = render(<Profile />);
-  const textTitle = getByTestId("text-title");
-  expect(textTitle.props.children).toContain("Perfil"); // Filho do elemento (tudo o que tem dentro da tag)
+  it("should exist title correctly", () => {
+    const { getByTestId } = render(<Profile />);
+    const textTitle = getByTestId("text-title");
+    expect(textTitle.props.children).toContain("Perfil"); // Filho do elemento (tudo o que tem dentro da tag)
+  });
 });
