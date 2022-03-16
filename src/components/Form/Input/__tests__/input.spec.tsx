a0becc8fb08beff73bcf203d7a39fcb596acb45e
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-03-16 10:46:21 -0300

    Chapter V - Testes Unitátios - Testes em Contexto

--- src/components/Form/Input/__tests__/input.spec.tsx
+++ src/components/Form/Input/__tests__/input.spec.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
+import { ThemeProvider } from "styled-components/native";
+import theme from "../../../../global/styles/theme";
 
 import Input from "../";
 
+const Providers: React.FC = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
 describe("Input Component", () => {
   it("must have specific border color when active", () => {
-    const { getByTestId } = render(
+    const { getByTestId, debug } = render(
       <Input
         testID="input-email"
         placeholder="E-mail"
         keyboardType="email-address"
         autoCorrect={false}
         active={true}
-      />
+      />,
+      {
+        wrapper: Providers,
+      }
     );
     const inputComponent = getByTestId("input-email");
-    expect(inputComponent.props.style[0].borderColor).toEqual("#e83F5B");
+    expect(inputComponent.props.style[0].borderColor).toEqual(
+      theme.colors.attention
+    );
+    expect(inputComponent.props.style[0].borderWidth).toEqual(3);
   });
 });
